8aeec32408243e3a254204598de11dfec5deec51
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-01-26 02:16:26 -0800

    some-code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // var num = 100
 
-// alert(--num) // - 1 
+// alert(--num) // - 1
 // alert(num--) // - 1
 // alert(num) // - 1
 // alert(++num) // 2
@@ -15,6 +15,88 @@
 // ++num // short
 // alert(num)
 
+// Not e
+// != !==
 
+// if (1 !== 5) { // true
+//     alert("Hello")
+// }
+
+// var a = prompt("kis chiz se chair banti he")
+
+// if (a !== "plastic") {
+//     alert("Correct")
+// }
+// if (a === "reet") {
+//     alert("Correct")
+// }
+
+// else{}
+
+// var a = "22"
+// if (a === 1) { // true
+//     alert("hello")
+// }
+// else if (a == 21) { // false
+//     alert("This is equals")
+// }
+// else if (a == 22) { // false
+//     alert("This is equals")
+// }
+// else if (a === 23) { // false
+//     alert("This is equals")
+// }
+// else if (a == 24) { // false
+//     alert("This is equals")
+// }
+// else {
+//     alert("Hello This condition is False")
+// }
+// Array.from()
+// var array = ["kashan", 1212, true, "adnan" ,]
+
+
+
+// alert(students)
+// console.log(students)
+// console.error("Rubaish is born")
+// console.warn("kashan")
+// console.info("aksdaksd")
+// console.log("\x1b[41m%s\x1b[2m", students)
+
+// students.push("hania")
+
+
+
+// students.push("Rubaish 4") // add in the last
+// students.pop() removes last element
+// students.unshift("Kashan") removes from the first
+// students.shift() // removes first element
+
+// tooka no.1  ❌❌
+
+// console.log(students)
+
+// var students = ["Azzan", "Adeeba", "Aniq", "Faham", "Rubaish", "Saad", "Fahad", "Bazeen"]
+
+// students.reverse()
+
+// console.log(students)
+
+// var name2 = ["K", "a", "s", "h", "a", "n"] // update change
+
+// var name3 = name2.join("") // return a new text
+
+// console.log(name3)
+
+
+// slice  
+
+// var students = ["Azzan", "Adeeba", "Aniq", "Faham", "Rubaish", "Saad", "Fahad", "Bazeen"]
+
+// var newArray = students.slice(0, 8)
+
+// console.log(newArray);
+// console.log(students);
 
 
