14cf50f6079bcbac29d146dee3aee3a23d8a9dfb
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-01-26 00:49:32 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// var num = 100
+
+// alert(--num) // - 1 
+// alert(num--) // - 1
+// alert(num) // - 1
+// alert(++num) // 2
+// alert(num++) // 2
+// alert(num)
+// num 1
+
+
+// num = num + 1 long
+// num++ // short
+// ++num // short
+// ++num // short
+// alert(num)
+
+
+
+
